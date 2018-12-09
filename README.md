308194f072d3dcb0686aaaacf638f94c73ff2e02
muchamadsyaifudin <45529748+muchamadsyaifudin@users.noreply.github.com>
2018-12-09 21:49:02 +0700

    Update and rename README.md to main.cpp

--- README.md
+++
@@ -1,2 +0,0 @@
-# -1-program-c-for
-program c++
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sampai = 0;
+	cout<<"lakukan penghitungan sampai :";cin>>sampai;
+	for(int i=1;i<=sampai;i++){
+		cout<<i<<endl;
+	}
+	return 0;
+}
